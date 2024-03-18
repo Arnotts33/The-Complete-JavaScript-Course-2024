3a9c0835c85c02e8f38e807a7db8339905576866
Arnaud Lahaut <arnaudlht@gmail.com>
2024-03-18 23:13:22 +0100

    Day one

---
+++ 01-Fundamentals-Part-1/starter/assignment.js
@@ -0,0 +1,15 @@
+let country = 'France';
+let continent = 'Europe';
+let population = 64000000;
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+let isIsland = false;
+let language;
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
